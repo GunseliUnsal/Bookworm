7e1bb21e0640f4238e26e1afa69db494a2839e35
Gunseli Unsal <gunseliunsal@gmail.com>
2024-08-04 13:08:50 +0300

    Adding a custom star rating component

---
+++ Bookworm/RatingView.swift
@@ -0,0 +1,18 @@
+//
+//  RatingView.swift
+//  Bookworm
+//
+//  Created by Günseli Ünsal on 4.08.2024.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RatingView()
+}
