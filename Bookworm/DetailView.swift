2b460c1369cd5353d0287e1385eac1231a235197
Gunseli Unsal <gunseliunsal@gmail.com>
2024-08-04 22:17:54 +0300

    add delete book option

--- Bookworm/DetailView.swift
+++ Bookworm/DetailView.swift
@@ -5,41 +5,61 @@
 //  Created by Günseli Ünsal on 4.08.2024.
 //
 
-import SwiftUI
 import SwiftData
+import SwiftUI
 
 struct DetailView: View {
+    @Environment(\.modelContext) var modelContext
+    @Environment(\.dismiss) var dismiss
+    @State private var showingDeleteAlert = false
+
     let book: Book
-    
+
     var body: some View {
-         NavigationStack{
-            ScrollView{
-                ZStack(alignment: .bottomTrailing){
-                    Image(book.genre)
-                        .resizable()
-                        .scaledToFit()
-                    Text(book.genre.uppercased())
-                        .fontWeight(.black)
-                        .padding(8)
-                        .foregroundStyle(.white)
-                        .background(.black.opacity(0.75))
-                        .clipShape(.capsule)
-                        .offset(x: -5, y: -5)
-                }
-                
-                Text(book.author)
-                    .font(.title)
-                    .foregroundStyle(.secondary)
-                
-                Text(book.review)
-                    .padding()
-                
-                RatingView(rating: .constant(book.rating)).font(.largeTitle)
+        ScrollView {
+            ZStack(alignment: .bottomTrailing) {
+                Image(book.genre)
+                    .resizable()
+                    .scaledToFit()
+
+                Text(book.genre.uppercased())
+                    .fontWeight(.black)
+                    .padding(8)
+                    .foregroundStyle(.white)
+                    .background(.black.opacity(0.75))
+                    .clipShape(.capsule)
+                    .offset(x: -5, y: -5)
+            }
+
+            Text(book.author)
+                .font(.title)
+                .foregroundStyle(.secondary)
+
+            Text(book.review)
+                .padding()
+
+            RatingView(rating: .constant(book.rating))
+                .font(.largeTitle)
+        }
+        .navigationTitle(book.title)
+        .navigationBarTitleDisplayMode(.inline)
+        .scrollBounceBehavior(.basedOnSize)
+        .alert("Delete book", isPresented: $showingDeleteAlert) {
+            Button("Delete", role: .destructive, action: deleteBook)
+            Button("Cancel", role: .cancel) { }
+        } message: {
+            Text("Are you sure?")
+        }
+        .toolbar {
+            Button("Delete this book", systemImage: "trash") {
+                showingDeleteAlert = true
             }
         }
-         .navigationTitle(book.title)
-         .navigationBarTitleDisplayMode(.inline)
-         .scrollBounceBehavior(.basedOnSize)
+    }
+
+    func deleteBook() {
+        modelContext.delete(book)
+        dismiss()
     }
 }
 
@@ -47,12 +67,11 @@ struct DetailView: View {
     do {
         let config = ModelConfiguration(isStoredInMemoryOnly: true)
         let container = try ModelContainer(for: Book.self, configurations: config)
-        let example = Book(title: "Text Title", author: "Test Author", genre: "Fantasy", review: "This was a great book. I really enjoyed it.", rating: 4)
-        
-        
-        return DetailView(book: example).modelContainer(container)
+        let example = Book(title: "Test Book", author: "Test Author", genre: "Fantasy", review: "This was a great book; I really enjoyed it.", rating: 4)
+
+        return DetailView(book: example)
+            .modelContainer(container)
     } catch {
-        return Text("Failed to create preview with error: \(error.localizedDescription)")
+        return Text("Failed to create preview: \(error.localizedDescription)")
     }
-    
 }
